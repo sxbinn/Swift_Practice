8a8bc3a7762597a976fd371458f8223bc13988aa
“sxbinn” <tnqls7307@gmail.com>
2026-04-09 01:31:16 +0900

    Initial Commit

---
+++ 06_ScoreKeeper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  06_ScoreKeeper
+//
+//  Created by kosoobin on 4/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 06_ScoreKeeper/_6_ScoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  _6_ScoreKeeperApp.swift
+//  06_ScoreKeeper
+//
+//  Created by kosoobin on 4/9/26.
+//
+
+import SwiftUI
+
+@main
+struct _6_ScoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
